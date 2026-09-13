016d2c619da892c366300f187a3f96d05bdf5365
Matheus Almeida <mathmatheus314@gmail.com>
2026-09-13 16:49:35 -0300

    fix: to csv and to .log

--- portscanner.py
+++ portscanner.py
@@ -17,26 +17,26 @@ def scan_ip(ip):
             # Tenta conectar no IP atual e na porta fixa
             if s.connect_ex((ip_str, PORT)) == 0:
                 print(f"[+] {ip_str} está com a porta {PORT} ABERTA")
-                # with open("ips.csv", mode="a") as arquivo:
-                #     arquivo.write(ip_str + ',\n')
+                with open("ips.csv", mode="a") as arquivo:
+                    arquivo.write(ip_str + ',\n')
     except Exception:
         pass
 
 def ips(ips):
     try:
-        # Converte a string da sub-rede em uma lista de hosts válidos
+        #Converte a string da sub-rede em uma lista de hosts válidos
         network = ipaddress.ip_network(ips, strict=False)
         hosts = list(network.hosts())
-        #print(f"Iniciando varredura na rede {ips} procurando a porta {PORT}...")
-        #print(f"Total de IPs a testar: {len(hosts)}")
-        #print("-" * 50)
+        print(f"Iniciando varredura na rede {ips} procurando a porta {PORT}...")
+        print(f"Total de IPs a testar: {len(hosts)}")
+        print("-" * 50)
 
         # Executa a varredura em paralelo usando Threads
         with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
             executor.map(scan_ip, hosts)
             
-        #print("-" * 50)
-        #print("Varredura concluída.")
+        print("-" * 50)
+        print("Varredura concluída.")
         
     except ValueError:
         print("Erro: Formato de sub-rede inválido. Use o formato CIDR (Ex: 192.168.1.0/24).")
