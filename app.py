2f9762fb14b312a522bc4c1f1ce206a015020573
Matheus Almeida <mathmatheus314@gmail.com>
2026-09-19 22:04:00 -0300

    feat: add multiprocessing and integration with dabase

--- app.py
+++ app.py
@@ -1,42 +1,55 @@
 # %%
-import json
 from librarys.minecraftping import MinecraftScan
 from librarys.portscanner import portScan
+from librarys.crud import database
+import multiprocessing
+import time
 
 
-#%%
-def to_json(HOST, PORT, resp):
-    #data = json.loads('{' + "\"server_info\": {" + f"\"ip\": \"{HOST}\", \"port\": {str(PORT)}" + "}," + resp[1:105] + '}')
-    data = json.loads(resp)
+#CONFIG
+SUBNET = "200"
+PORT = "25565"
+TIMEOUT = 5
+MAX_WORKERS = 100
 
-    with open("./servers/ips.json", "a", encoding="utf-8") as arquivo:
-        arquivo.write(json.dumps(data, indent=2, ensure_ascii=False) + ',\n')
-
-def to_txt(HOST:str, PORT:int, resp):
-    data = f"{HOST}:{PORT} : " + resp[:resp.find("favicon")]
-    with open("./servers/minecraft.txt", "a", encoding="utf-8") as arquivo:
-        arquivo.write(data + ',\n')
-# %%
-lines = []
-with open("./servers/ips.csv", "r") as fl:
-    lines  = fl.readlines()
-
-for i in range(len(lines)):
-    lines[i] = lines[i].replace('\n', '').replace(',','')
-
-lines
-# %%
-minecraftPing = MinecraftScan()
-PORT = 25565
-for HOST in (lines):
-    resp = minecraftPing.client(HOST, PORT)
-    if resp != None:
-        to_txt(HOST, PORT, resp)
-    
+#DATABASE
+db = database()
 
 # %%
-# Configurações do Alvo
-SUBNET = "200"  # Defina a faixa de rede (CIDR)
-PORT = 25565                 # Defina a porta única que deseja escanear
-TIMEOUT = 1.0              # Tempo de espera por resposta (em segundos)
-MAX_WORKERS = 100          # Número de testes em paralelo
+
+# def setup():
+#     with open("./servers/minecraft.txt", "w", encoding="utf-8") as file:
+#         print("Create minecraft txt")
+
+def scan_ips():
+    print("Iniciando o scan de ips e portas")
+    pScan = portScan(SUBNET,PORT, TIMEOUT, MAX_WORKERS)
+    address = pScan.start()
+    db.create(address[0], address[1])
+
+def ping_ips():
+    ping = MinecraftScan()
+    while(1):
+        data = db.select()
+        print(data)
+        for x in data:
+            print(x["_id"],x["ip"], x["port"])
+            res = ping.client(x["ip"], x["port"])
+            if res != None:
+                with open("./servers/minecraft.txt", "a") as fl:
+                    fl.write(res + "\n")
+            db.delete(x["_id"])
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    print("Iniciando Script")
+    p1 = multiprocessing.Process(target=scan_ips)
+    p2 = multiprocessing.Process(target=ping_ips)
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+# %%
